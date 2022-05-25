fae7a09d66852df65be749eb1b2be77bd759f1e8
NetraMehta <78476060+NetraMehta@users.noreply.github.com>
2022-05-25 15:58:19 +0530

    Add files via upload

--- UploadFiles.py
+++ UploadFiles.py
@@ -1,21 +1,37 @@
+import os
 import dropbox
+from dropbox.files import WriteMode
 
 class TransferData:
-    def __init__(self, access_token):
-        self.access_token = access_token
+    def __init__(self,access_token):
+        self.access_token =  access_token
 
     def upload_file(self, file_from, file_to):
         dbx = dropbox.Dropbox(self.access_token)
-        f = open(file_from, 'rb')
-        dbx.files_upload(f.read(), file_to)
+
+            # enumerate local files recursively
+        for root, dirs, files in os.walk(file_from):
+
+            for filename in files:
+                    # construct the full local path
+                local_path = os.path.join(root, filename)
+
+                    # construct the full Dropbox path
+                relative_path = os.path.relpath(local_path, file_from)
+                dropbox_path = os.path.join(file_to, relative_path)
+                    # upload the file
+                with open(local_path, 'rb') as f:
+                    dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
 
 def main():
     access_token = 'sl.BIBd8EoyRxyWNi1gKEr1xk4KuBLYrco0JBpqvrGzcErNrU_LU3LphcVCpW653MabBrno3w8qm0hMeEW069MxSMx20z7tXotTgybIsDO_K4Ykds7ZX4xJCOGVwqcKdj1pg5uha29WlVE'
     transferData = TransferData(access_token)
-    file_from = input('Enter the file path to transfer: ')
-    file_to = input('Enter the full path to upload to dropbox: ')
 
-    transferData.upload_file(file_from, file_to)
-    print('File has been moved')
+    file_from = str(input("Enter the folder path to transfer : -"))
+    file_to = input("enter the full path to upload to dropbox:- ")  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
+
+    # API v2
+    transferData.upload_file(file_from,file_to)
+    print("file has been moved !!!")
 
 main()
